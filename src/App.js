033f8087b30d20c05afd113d8d615db5d37d2295
Animesh59 <abelieve0304@gmail.com>
2024-07-23 16:02:50 +0530

    v6 Routes is used

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 import './App.css';
+import Navbar from './component/Navbar';
+import Home from "./component/Home";
+import About from "./component/About";
+import TextField from './component/TextField';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<> <Home /> </>} exact />
+            <Route path='/about' element={<> <About /> </>} exact />
+            <Route path='/services' element={<> <TextField /> </>} exact />
+          </Routes>
+        </Router >
+      </div>
+    </>
   );
 }
 
---
+++ src/component/TextField.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { Navigate } from "react-router-dom";
+import axiosClient from "./axiosClient";
+
+export default function TextField() {
+    function handleUserInput(event) {
+        setText(event.target.value)
+    }
+    function handleUpper() {
+        setText(text.toUpperCase())
+    }
+    function handleLower() {
+        setText(text.toLowerCase())
+    }
+    const [text, setText] = useState()
+
+    // axiosClient.get('/auth')
+    //     .then(res => {
+    //         console.log(res.data.status);
+    //         if (res.data.status == 'success') {
+    //             console.log('inside');
+    //             return (<><h1>can not</h1></>)
+    //         }
+    //         return (
+    //             <>
+    //                 <div className="mt-5">
+    //                     <textarea value={text} placeholder="Enter your text here" onChange={handleUserInput} className="form-control mt-10" id="exampleFormControlTextarea1" rows="08"></textarea>
+    //                 </div>
+    //                 <button type="button" onClick={handleUpper} className="btn btn-primary mt-3 mb-3">upper</button>
+    //                 <button type="button" onClick={handleLower} className="btn btn-primary mt-3 mb-3 ms-3">lower</button>
+    //             </>
+    //         )
+    //     })
+
+    // setTimeout(() => {
+    //     console.log('Hello I am React');
+    return (
+        <>
+            <div className="mt-5">
+                <textarea value={text} placeholder="Enter your text here" onChange={handleUserInput} className="form-control mt-10" id="exampleFormControlTextarea1" rows="08"></textarea>
+            </div>
+            <button type="button" onClick={handleUpper} className="btn btn-primary mt-3 mb-3">upper</button>
+            <button type="button" onClick={handleLower} className="btn btn-primary mt-3 mb-3 ms-3">lower</button>
+        </>
+    )
+    // }, 5000);
+}
+
