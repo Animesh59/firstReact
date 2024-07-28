e87ef0107a0c7d7e1d6a8dd9ea8712444a866b88
Animesh59 <abelieve0304@gmail.com>
2024-07-28 19:46:32 +0530

    mobile responsive

--- src/App.js
+++ src/App.js
@@ -43,41 +43,36 @@ function App() {
     <>
       <head>
         <meta charset="utf-8" />
-        {/* <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />
         <meta name="description" content="Web site created using create-react-app" />
-        <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
-        <link rel="manifest" href="%PUBLIC_URL%/manifest.json" /> */}
+        <link rel="apple-touch-icon" href="/logo192.png" />
+        <link rel="manifest" href="/manifest.json" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossOrigin="anonymous" />
-        <title>{title}</title>
-      </head>
-
-      <body data-bs-theme={mode}>
-        <noscript>You need to enable JavaScript to run this app.</noscript>
-        <div>
-          {/* <h1>Hello from body</h1> */}
-          <Router>
-            <Navbar mode={mode} textcolor={textcolor} toggleMode={toggleMode} msg={msg} />
-            <div className="container">
-              <Switch>
-                <Route exact path='/'>
-                  {console.log('root page')}
-                  <Home />
-                </Route>
-                <Route path='/about'>
-                  <About />
-                </Route>
-                <Route path='/services'><TextField />
-                </Route>
-              </Switch>
-            </div>
-          </Router >
-        </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
           crossOrigin="anonymous"></script>
+        <title>{title}</title>
+      </head>
+      <body data-bs-theme={mode}>
+        <Router>
+          <Navbar mode={mode} textcolor={textcolor} toggleMode={toggleMode} msg={msg} />
+          <div className="container">
+            <Switch>
+              <Route exact path='/'>
+                {console.log('root page')}
+                <Home />
+              </Route>
+              <Route path='/about'>
+                <About />
+              </Route>
+              <Route path='/services'><TextField />
+              </Route>
+            </Switch>
+          </div>
+        </Router >
       </body>
     </>
   );
